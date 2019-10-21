997267aef3b5d8763b14f515a7df196ae151d256
azahrx <56218043+azahrx@users.noreply.github.com>
2019-10-21 11:51:03 +0700

    Add files via upload

---
+++ ghost-followers.py
@@ -0,0 +1,28 @@
+import instaloader
+import pandas as pd
+
+L = instaloader.Instaloader()
+
+L.login('USER', 'PASSWORD')
+
+USER = 'your_target'
+PROFILE = USER
+
+profile = instaloader.Profile.from_username(L.context, PROFILE)
+
+# Menghitung jumlah likes
+likes = set()
+print('Jumlah like dalam profil {}.'.format(profile.username))
+for post in profile.get_posts():
+    print(post)
+    likes = likes | set(post.get_likes())
+
+print('Jumlah followers dalam profil {}.'.format(profile.username))
+followers = set(profile.get_followers())
+
+# Hasil ghost followers dari like terendah
+ghosts = followers - likes
+print('Ambil ghost followers.')
+with open('ghostfol.txt', 'w') as f:
+    for ghost in ghosts:
+        print(ghost.username, file=f)
