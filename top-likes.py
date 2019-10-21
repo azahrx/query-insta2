3bd4b646c990771ee40d2700900bbb4f26bda0f7
azahrx <56218043+azahrx@users.noreply.github.com>
2019-10-21 11:52:47 +0700

    Add files via upload

---
+++ top-likes.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import pymongo
+from instaloader import Instaloader, Profile
+
+PROFILE = 'your_username'
+
+L = Instaloader()
+
+profile = Profile.from_username(L.context, PROFILE)
+
+# Mengurutkan post berdasarkan banyaknya likes
+posts_sorted_by_likes = sorted(profile.get_posts(), key=lambda post: post.likes, reverse=True)
+
+for i in posts_sorted_by_likes:
+   print(i)
+with open('toplikes.txt', 'w') as f:
+    for i in posts_sorted_by_likes:
+        print(i, file=f)
